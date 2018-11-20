7bffbe942b3c369084810d794e8546d4460f011f
Kara Bayse <karabayse@gmail.com>
2018-11-20 15:40:47 -0600

    added base js

---
+++ client.js
@@ -0,0 +1,17 @@
+$(function() {
+
+	$(".add-item").on('click', function(event) {
+		  event.preventDefault();
+      let listItem = '';
+		  let itemHtml = "<li><span class='item-check'></span><span class='item-text'>" + listItem + "</span><span class='item-remove'></span></li>";
+	});
+
+	$(".shopping-list").on('click', '.item-remove', function(event) {
+
+	});
+
+	$(".shopping-list").on('click', '.item-check', function(event) {
+
+	});
+
+}); // end function
